bce0a62268701acd595a769f3f3c3b8c8c18f252
Nishan <maharjannishan459@gmail.com>
2026-06-29 21:43:41 +0545

    Templates

--- admin_nishan/views.py
+++ admin_nishan/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def admin_dashboard(request):
+    return render(request, "admin_dashboard.html")
+
+def admin_accounts(request):
+    return render(request, "admin_accounts.html")
+
+def admin_appointments(request):
+    return render(request, "admin_appointments.html")
+
+def admin_manage_doctor(request):
+    return render(request, "admin_manage_doctor.html")
+
+def admin_manage_patients(request):
+    return render(request, "admin_manage_patients.html")
+
+def admin_reports(request):
+    return render(request, "admin_reports.html")
--- hospital/urls.py
+++ hospital/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),              # Keep this if you want Django Admin
+    path("admin-panel/", include("admin_nishan.urls")),
 ]
