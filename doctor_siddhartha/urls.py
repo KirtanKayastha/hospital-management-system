eedab01346db2ff3f4cd69af6a754ba2b9eab93f
KirtanKayastha <kirtankayastha@gmail.com>
2026-06-30 11:12:48 +0545

    Fixed doctor templates path - added doc_siddhartha/ subfolder

--- doctor_siddhartha/urls.py
+++ doctor_siddhartha/urls.py
@@ -4,8 +4,8 @@ from django.views.generic import TemplateView
 app_name = 'doctor_siddhartha'
 
 urlpatterns = [
-    path('dashboard/', TemplateView.as_view(template_name='doctor_dashboard.html'), name='dashboard'),
-    path('schedule/', TemplateView.as_view(template_name='schedule.html'), name='schedule'),
-    path('patients/', TemplateView.as_view(template_name='patients_records.html'), name='patients'),
-    path('prescriptions/', TemplateView.as_view(template_name='prescription.html'), name='prescriptions'),
+    path('dashboard/', TemplateView.as_view(template_name='doc_siddhartha/doctor_dashboard.html'), name='dashboard'),
+    path('schedule/', TemplateView.as_view(template_name='doc_siddhartha/schedule.html'), name='schedule'),
+    path('patients/', TemplateView.as_view(template_name='doc_siddhartha/patients_records.html'), name='patients'),
+    path('prescriptions/', TemplateView.as_view(template_name='doc_siddhartha/prescription.html'), name='prescriptions'),
 ]
