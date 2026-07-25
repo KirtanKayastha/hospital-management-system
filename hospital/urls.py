5671e76efd898deccdeeaf474ffba393c9016510
KirtanKayastha <kirtankayastha@gmail.com>
2026-07-25 11:58:34 +0545

    Db testing

--- hospital/urls.py
+++ hospital/urls.py
@@ -4,6 +4,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+from django.http import HttpResponse
+from django.db import connection
+
+def db_check(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT current_database();")
+        db_name = cursor.fetchone()
+    return HttpResponse(f"✅ Connected to database: {db_name[0]}")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
