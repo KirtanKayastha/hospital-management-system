9be55847ec8d9e9f96bf9934a6986a058e7166b3
KirtanKayastha <kirtankayastha@gmail.com>
2026-07-25 11:40:39 +0545

    Auto-create admin user on deploy

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+# Auto-create superuser
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', '[REDACTED EMAIL]', '[REDACTED PASSWORD]')" | python manage.py shell
