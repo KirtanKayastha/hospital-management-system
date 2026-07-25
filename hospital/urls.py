148b59c2740201c43e64e39b2c035993d94600fd
KirtanKayastha <kirtankayastha@gmail.com>
2026-07-25 11:30:29 +0545

    Fix URL routing for all modules

--- hospital/urls.py
+++ hospital/urls.py
@@ -2,9 +2,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('auth_kirtan.urls', namespace='auth_kirtan')),
-    path('patient/', include('patient_roshan.urls', namespace='patient_roshan')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # Home page (root URL)
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    
+    # Auth routes (login, register, logout)
+    path('', include('auth_kirtan.urls')),
+    
+    # Patient module
+    path('patient/', include('patient_roshan.urls')),
+    
+    # Doctor module
+    path('doctor/', include('doctor_siddhartha.urls')),
+    
+    # Admin module
+    path('admin-panel/', include('admin_nishan.urls')),
+]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
