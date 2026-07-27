7a882a12dc7c6e7e3e67c69eea557e656d860029
Nishan <maharjannishan459@gmail.com>
2026-07-27 22:51:29 +0545

    Complete admin patient and doctor management

---
+++ admin_nishan/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from patient_roshan.models import PatientProfile
+
+
+class PatientForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150)
+    email = forms.EmailField()
+
+    class Meta:
+        model = PatientProfile
+        fields = [
+            "phone",
+            "dob",
+            "gender",
+            "blood_group",
+            "address",
+            "emergency_contact_name",
+            "emergency_contact_relation",
+            "emergency_contact_phone",
+            "profile_picture",
+        ]
+        widgets = {
+            "dob": forms.DateInput(attrs={"type": "date"}),
+            "address": forms.Textarea(attrs={"rows": 3}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance and self.instance.user:
+            self.fields["first_name"].initial = self.instance.user.first_name
+            self.fields["last_name"].initial = self.instance.user.last_name
+            self.fields["email"].initial = self.instance.user.email
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+
+        user = profile.user
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+
+        if commit:
+            user.save()
+            profile.save()
+
+        return profile
