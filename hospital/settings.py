4322a886a293aa65b8a7eb44d13b8a70d1d09812
ANT7-Tech <auronextech41@gmail.com>
2026-07-28 13:40:33 +0545

    fix: DEBUG True for local media serving - Roshan Ansari

--- hospital/settings.py
+++ hospital/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '[REDACTED SECRET_KEY]')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
