327ea8c2e39173b56bfe58fe623c39a9bb97dd4e
Nishan <maharjannishan459@gmail.com>
2026-07-28 12:35:56 +0545

    Change PW, Edit, Delete, Enable / disable Login

--- hospital/settings.py
+++ hospital/settings.py
@@ -10,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-wpk!9)59onq%^01logxbyu2dmz5w^1cw75l$sda%x6lqxtqbkl')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
