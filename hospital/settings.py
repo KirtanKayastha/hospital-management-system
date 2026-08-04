22854b9621254a1fe29c5afe3bcca35c4cf9654e
KirtanKayastha <kirtankayastha@gmail.com>
2026-08-04 09:16:17 +0545

    Configure Mailgun SMTP for password reset emails

--- hospital/settings.py
+++ hospital/settings.py
@@ -107,12 +107,12 @@ LOGOUT_REDIRECT_URL = '/login/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Email Configuration (Gmail SMTP)
+# Email Configuration (Mailgun SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.mailgun.org')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER or 'noreply@medcore.com')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'sachit@sandbox22b3bf72b56e47c986cf76d90c69cb34.mailgun.org')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '06ab56c41cd55f284a24d1758ce91b0f-d3a6f780-7414eacd')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
