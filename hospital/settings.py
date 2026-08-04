074305bd2f471f49db0975b057e533d59c9092cc
KirtanKayastha <kirtankayastha@gmail.com>
2026-08-04 09:35:03 +0545

    Configure Mailgun API via django-anymail for password reset emails

--- hospital/settings.py
+++ hospital/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'patient_roshan',
     'doctor_siddhartha',
     'admin_nishan',
+    'anymail',
 ]
 
 MIDDLEWARE = [
@@ -107,12 +108,10 @@ LOGOUT_REDIRECT_URL = '/login/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Email Configuration (Mailgun SMTP)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.mailgun.org')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '[REDACTED MAILGUN SMTP USER]')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '[REDACTED MAILGUN SMTP PASSWORD]')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+# Email Configuration (Mailgun via django-anymail)
+EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '[REDACTED MAILGUN SMTP USER]')
+ANYMAIL = {
+    'MAILGUN_API_KEY': os.environ.get('MAILGUN_API_KEY', '[REDACTED MAILGUN API KEY]'),
+    'MAILGUN_SENDER_DOMAIN': os.environ.get('MAILGUN_SENDER_DOMAIN', '[REDACTED MAILGUN DOMAIN]'),
+}
