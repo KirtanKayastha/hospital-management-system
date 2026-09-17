92b02d820c9f15128b6b4f45bc701946a105ad20
KirtanKayastha <kirtankayastha@gmail.com>
2026-09-17 14:31:40 +0545

    Add admin superuser creation via environment variables in build.sh

--- build.sh
+++ build.sh
@@ -18,4 +18,28 @@ python manage.py collectstatic --no-input
 echo "[BUILD] Running migrations..."
 python manage.py migrate
 
+echo "[BUILD] Creating superuser (if ADMIN_USERNAME is set)..."
+if [ -n "$ADMIN_USERNAME" ] && [ -n "$ADMIN_EMAIL" ] && [ -n "$ADMIN_PASSWORD" ]; then
+    python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = '$ADMIN_USERNAME'
+email = '$ADMIN_EMAIL'
+password = '$ADMIN_PASSWORD'
+if User.objects.filter(username=username).exists():
+    u = User.objects.get(username=username)
+    u.set_password(password)
+    u.is_staff = True
+    u.is_superuser = True
+    u.is_active = True
+    u.save()
+    print(f'Superuser \"{username}\" updated.')
+else:
+    User.objects.create_superuser(username, email, password)
+    print(f'Superuser \"{username}\" created.')
+"
+else
+    echo "[BUILD] ADMIN_USERNAME/EMAIL/PASSWORD not set; skipping superuser creation."
+fi
+
 echo "[BUILD] Build complete!"
